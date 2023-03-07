c54b1a93baab112495b70cd0b99219b04dab6403
tan <tancodes@proton.me>
2023-03-06 19:11:56 -0700

    add custom status

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,12 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`${client.user.tag} is ready to rumble`);
+		client.user.setActivity('russia discourse', { type: ActivityType.Watching });
+		
 	},
 };
+
